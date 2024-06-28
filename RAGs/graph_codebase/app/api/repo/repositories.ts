58512a4c0ff5e3abbef779dd2b4da47b12aa8b72
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-06-28 16:07:16 -0700

    added other repo and tested graph rag successfully for python codebase

--- RAGs/graph_codebase/app/api/repo/repositories.ts
+++ RAGs/graph_codebase/app/api/repo/repositories.ts
@@ -1,4 +1,5 @@
 export const RESPOSITORIES = [
+    "https://github.com/pallets/flask",
     "https://github.com/falkordb/falkordb-py",
     "https://github.com/pallets/click",
     "https://github.com/psf/requests"
